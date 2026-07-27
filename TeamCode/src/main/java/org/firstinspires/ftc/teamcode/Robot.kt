6056970da79f03c7466ba3b5543f8c0b6cc2b175
Julian B. <a01714453@.tec.mx>
2026-07-27 15:30:11 -0600

    Base code for the Academy Bot, does not contain any subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.teamcode.utils.extensions.onTrue
 class Robot(private val alliance: Alliance, private val hardwareMap: HardwareMap, private val controller: GamepadEx, telemetry: Telemetry): TecDroidRobot(telemetry, hardwareMap) {
 
     /* Declare your subsystems here */
-    private lateinit var continuousRotationServoExample: CRServoEx
+
 
     init {
         subsystemInitialization()
@@ -24,16 +24,10 @@ class Robot(private val alliance: Alliance, private val hardwareMap: HardwareMap
 
     /* Initialize your subsystems here */
     override fun subsystemInitialization() {
-        continuousRotationServoExample = CRServoEx(hardwareMap, "servo")
-        continuousRotationServoExample.setRunMode(Motor.RunMode.RawPower)
-        continuousRotationServoExample.inverted = false
     }
 
     /* Initialize your teleop controller commands here */
     override fun initTeleOp() {
-        controller.a()
-            .onTrue(InstantCommand({ continuousRotationServoExample.set(1.0) }))
-            .onFalse(InstantCommand({ continuousRotationServoExample.set(0.0) }))
     }
 
     /* Initialize your auto commands here, set chassis alliance and starting pose */
